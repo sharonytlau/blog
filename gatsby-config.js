45fab5cedb6ea09e85f3fdc4824daa49a4d7b50a
sharonytlau <sharonytlau@gmail.com>
2022-02-19 23:21:18 +0800

    Clear unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,22 +38,16 @@ module.exports = {
     },
     `gatsby-plugin-image`,
     // {
-    //   resolve: `gatsby-plugin-typography`,
+    //   resolve: `gatsby-plugin-google-fonts`,
     //   options: {
-    //     pathToConfigModule: `src/utils/typography`,
+    //     fonts: [
+           // `Montserrat\:700,900`,
+    //       `Merriweather\:400,400i,700,700i`,
+    //       `Noto Sans Mono\:400,400i,700,700i`,
+    //     ],
+    //     display: "swap",
     //   },
     // },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Montserrat\:700,900`,
-          `Merriweather\:400,400i,700,700i`,
-          `Noto Sans Mono\:400,400i,700,700i`,
-        ],
-        display: "swap",
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -94,65 +88,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  nodes {
-                    excerpt
-                    html
-                    fields {
-                      slug
-                    }
-                    frontmatter {
-                      title
-                      date
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
-          },
-        ],
-      },
-    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
